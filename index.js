604f2652d7ce13c1f6ca7028310c2df892a774dc
golbatjenkins <121843630+golbatjenkins@users.noreply.github.com>
2023-01-02 23:20:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,198 @@
-const http = require('http'),
-    https = require('https'),
-    fs = require('fs'),
-    config = require('./config.json'),
-    proxy = new (require('./lib/index'))(config.prefix, {
-      localAddress: config.localAddresses ? config.localAddresses : false,
-      blacklist: config.blockedHostnames ? config.blockedHostnames : false
-    }),
-    index_file = 'index.html',
-    atob = str => new Buffer.from(str, 'base64').toString('utf-8'),
-    app = (req, res) => {
+const upstream = 'www.google.com'
 
-      // HTTP(S) proxy.
-      if (req.url.startsWith(config.prefix)) return proxy.http(req, res); 
+// 目标网站路径
+const upstream_path = '/'
 
-      req.pathname = req.url.split('#')[0].split('?')[0];
-      req.query = {};
-      req.url.split('#')[0].split('?').slice(1).join('?').split('&').forEach(query => req.query[query.split('=')[0]] = query.split('=').slice(1).join('='));
+// 打算为使用移动设备的用户检索的网站.
+const upstream_mobile = 'google.com'
 
-      if (req.query.url && (req.pathname == '/prox' || req.pathname == '/prox/' || req.pathname == '/session' || req.pathname == '/session/')) {
-        var url = atob(req.query.url);
+// 您希望暂停服务的国家和地区
+const blocked_region = []
 
-        if (url.startsWith('https://') || url.startsWith('http://')) url = url;
-        else if (url.startsWith('//')) url = 'http:' + url;
-        else url = 'http://' + url;
+// 要阻止其使用服务的IP地址
+const blocked_ip_address = ['0.0.0.0', '127.0.0.1']
 
-        return (res.writeHead(301, { location: config.prefix + proxy.proxifyRequestURL(url) }), res.end(''));
-      }
+// 上游地址是否使用HTTPS协议
+const https = true
+
+// 是否禁用缓存
+const disable_cache = false
+
+// 替换文本
+//格式：  '被替换':'替换'
+//注意js结束的的逗号
+const replace_dict = {
+    'www.google.com':'yourworkername.yourusername.workers.dev'
+}
+
+let data={}
+addEventListener('fetch', event => {
+    event.respondWith(fetchAndApply(event.request));
+})
+
+async function fetchAndApply(request) {
+    const region = request.headers.get('cf-ipcountry').toUpperCase();
+    const ip_address = request.headers.get('cf-connecting-ip');
+    const user_agent = request.headers.get('user-agent');
+
+    let response = null;
+    let url = new URL(request.url);
+    let url_hostname = url.hostname;
+
+    if (https == true) {
+        url.protocol = 'https:';
+    } else {
+        url.protocol = 'http:';
+    }
+
+    if (await device_status(user_agent)) {
+        var upstream_domain = upstream;
+    } else {
+        var upstream_domain = upstream_mobile;
+    }
+
+    url.host = upstream_domain;
+    if (url.pathname == '/') {
+        url.pathname = upstream_path;
+    } else {
+        url.pathname = upstream_path + url.pathname;
+    }
+
+    if (blocked_region.includes(region)) {
+        response = new Response('拒绝访问: 您所在的地区不可用!', {
+            status: 403
+        });
+    } else if (blocked_ip_address.includes(ip_address)) {
+        response = new Response('拒绝访问: 您的IP地址在Ban列表中!', {
+            status: 403
+        });
+    } else {
+        let method = request.method;
+        let request_headers = request.headers;
+        let new_request_headers = new Headers(request_headers);
+
+        new_request_headers.set('Host', upstream_domain);
+        new_request_headers.set('Referer', url.protocol + '//' + upstream_domain);
+
+        
+
+        if(method == 'POST'){
+            let origin_formData = await request.text()
+            data = {
+                method: method,
+                headers: new_request_headers,
+                body : origin_formData
+            }
+            console.log(data)
+        }else if(method == 'GET'){
+            data = {
+                method: method,
+                headers: new_request_headers
+            }
+        }
+        let fuckKeys =  Object.fromEntries(new_request_headers)
+        console.log(method)
+        console.log(fuckKeys)
+        let original_response = await fetch(url.href, data)
+
+        connection_upgrade = new_request_headers.get("Upgrade");
+        if (connection_upgrade && connection_upgrade.toLowerCase() == "websocket") {
+            return original_response;
+        }
+
+        let original_response_clone = original_response.clone();
+        let original_text = null;
+        let response_headers = original_response.headers;
+        let new_response_headers = new Headers(response_headers);
+        let status = original_response.status;
+		
+		if (disable_cache) {
+			new_response_headers.set('Cache-Control', 'no-store');
+	    }
+
+        new_response_headers.set('access-control-allow-origin', '*');
+        new_response_headers.set('access-control-allow-credentials', true);
+        new_response_headers.delete('content-security-policy');
+        new_response_headers.delete('content-security-policy-report-only');
+        new_response_headers.delete('clear-site-data');
+		
+		if (new_response_headers.get("x-pjax-url")) {
+            new_response_headers.set("x-pjax-url", response_headers.get("x-pjax-url").replace("//" + upstream_domain, "//" + url_hostname));
+        }
+		
+        const content_type = new_response_headers.get('content-type');
+        if (content_type != null && content_type.includes('text/html') && content_type.includes('UTF-8')) {
+            original_text = await replace_response_text(original_response_clone, upstream_domain, url_hostname);
+        } else {
+            original_text = original_response_clone.body
+        }
+		
+        response = new Response(original_text, {
+            status,
+            headers: new_response_headers
+        })
+    }
+    
+    return response;
+}
+
+async function replace_form_data(formData, upstream_domain, host_name) {
+    let text = await response.text()
+
+    var i, j;
+    for (i in replace_dict) {
+        j = replace_dict[i]
+        if (i == '$upstream') {
+            i = upstream_domain
+        } else if (i == '$custom_domain') {
+            i = host_name
+        }
+
+        if (j == '$upstream') {
+            j = upstream_domain
+        } else if (j == '$custom_domain') {
+            j = host_name
+        }
+
+        let re = new RegExp(i, 'g')
+        text = text.replace(re, j);
+    }
+    return text;
+}
+
+async function replace_response_text(response, upstream_domain, host_name) {
+    let text = await response.text()
+
+    var i, j;
+    for (i in replace_dict) {
+        j = replace_dict[i]
+        if (i == '$upstream') {
+            i = upstream_domain
+        } else if (i == '$custom_domain') {
+            i = host_name
+        }
+
+        if (j == '$upstream') {
+            j = upstream_domain
+        } else if (j == '$custom_domain') {
+            j = host_name
+        }
+
+        let re = new RegExp(i, 'g')
+        text = text.replace(re, j);
+    }
+    return text;
+}
 
 
-      // General file server.
-      const publicPath = __dirname + '/public' + req.pathname;
-
-      const error = () => (res.statusCode = 404, res.end(fs.readFileSync(__dirname + '/lib/error.html', 'utf-8').replace('%ERR%', `Cannot ${req.method} ${req.pathname}`)))
-
-      fs.lstat(publicPath, (err, stats) => {
-
-        if (err) return error();
-
-        if (stats.isDirectory()) fs.existsSync(publicPath + index_file) ? fs.createReadStream(publicPath + index_file).pipe(res) : error();
-        else if (stats.isFile()) !publicPath.endsWith('/') ? fs.createReadStream(publicPath).pipe(res) : error();
-        else error();
-
-      });
-
-    },
-    server = config.ssl ? https.createServer({key: fs.readFileSync('./ssl/default.key'), cert: fs.readFileSync('./ssl/default.crt')}, app) : http.createServer(app);
-
-// Websocket proxy.
-proxy.ws(server);
-
-server.listen(process.env.PORT || config.port, () => console.log(`${config.ssl ? 'https://' : 'http://'}0.0.0.0:${config.port}`))
-
-
-
+async function device_status(user_agent_info) {
+    var agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < agents.length; v++) {
+        if (user_agent_info.indexOf(agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
